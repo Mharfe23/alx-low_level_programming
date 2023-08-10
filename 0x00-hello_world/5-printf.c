37b898242debf92fc4b50e2080a0c89d70946baa
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-10 16:18:06 -0700

    f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ * main - whatever
+ * Description:blablabla
+ * section header:ldlad
+ * Return:0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
