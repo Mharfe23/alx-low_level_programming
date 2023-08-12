39ac368669618197a8bcc185b80cb77e0fb11bd2
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-12 16:55:44 -0700

    z

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - whatever
+ * Description:blablabla
+ * section header:ldlad
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	putchar('\n');
+	return (0);
+}
