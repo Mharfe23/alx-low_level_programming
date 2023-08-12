31c6f96f1ed1502e6b032de2b9b75282594c9e7c
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-12 16:39:19 -0700

    z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - whatever
+ * Description:blablabla
+ * section header:ldlad
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+	for (i = 65; i < 90; i++)
+        {
+                putchar(i);
+        }
+
+        
+	return (0);
+}
