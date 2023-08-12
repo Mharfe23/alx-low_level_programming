e560b3f66dc0b2ebd2e4848b95177fb0ecb3ff66
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-12 16:46:33 -0700

    z

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -15,12 +15,10 @@ int main(void)
 	{
 		putchar(i);
 	}
-
-	for (i = 65; i < 90; i++)
+	for (i = 65; i < 91; i++)
         {
                 putchar(i);
         }
 	putchar('\n');
-
 	return (0);
 }
