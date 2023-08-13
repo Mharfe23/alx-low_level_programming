000b820579deb7ba32fbde93a3d70ee231935f20
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-13 06:41:52 -0700

    l

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - whatever
+ * Description:blablabla
+ * section header:ldlad
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 1;i <= 9; i++)
+		putchar(i + '0');
+		putchar(', ');
+	return (0);
+
+
+}
