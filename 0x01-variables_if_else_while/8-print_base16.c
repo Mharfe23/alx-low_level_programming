8908b5f802930cc302c7eea18406843530ba547b
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-13 05:22:40 -0700

    m

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - whatever
+ * Description:blablabla
+ * section header:ldlad
+ * Return:0
+ */
+int main(void)
+{
+	char c;
+
+	int i;
+
+	for (i = 48;i =<57; i++)
+		putchar(i);
+	for (c = 'a'; c = 'f'; c++)
+		putchar(i);
+	putchar('\n');
+}
