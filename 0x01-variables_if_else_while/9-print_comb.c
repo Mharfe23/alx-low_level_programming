6ad5865a0db154b09fd952c043b5ea18148b01d5
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-13 07:13:46 -0700

    m

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,16 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i <= 56 ; i++)
+	for (i = 48; i <= 57 ; i++)
 	{
 		putchar(i);
-		putchar(',');
-		putchar(' ');
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar(57);
+	putchar(' ');
 	return (0);
 
 }
