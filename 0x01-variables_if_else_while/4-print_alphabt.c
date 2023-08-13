0849df8de0e96ff81927fa8e3adbbc5f5f9a35a7
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-13 04:59:47 -0700

    m

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - whatever
+ * Description:blablabla
+ * section header:ldlad
+ * Return:0
+ */
+int main(void)
+{
+	for (i = 97; i < 123; i++)
+	{
+		if (i == 113 && i == 101)
+			putchar('');
+		putchar(i)
+	}
+}
