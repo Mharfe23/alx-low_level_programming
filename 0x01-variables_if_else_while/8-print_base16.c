7a0614a16329d8b2bfdd9d16c1345206a470e061
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-13 06:28:39 -0700

    l

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,13 +14,9 @@ int main(void)
 	int i;
 
 	for (i = 48; i < 58; i++)
-	{
 		putchar(i);
-	}
-	for (c = 'a'; c <= 'f'; c++)
-	{
+	for (c = 'a'; c = 'f'; c++)
 		putchar(i);
-	}
 	putchar('\n');
 	return (0);
 }
