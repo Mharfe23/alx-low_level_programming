d5209b154db51549e044464bdeb5e80a0b80a1ed
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-16 13:09:42 -0700

    g

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * print_alphabet-print alphabet a to z
+ * description:priting alphabet
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i =<122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
