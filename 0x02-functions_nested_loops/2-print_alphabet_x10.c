4191f0d84844c30aa613717116aec72329d89f1d
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-16 14:38:11 -0700

    f

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+
 /*
  * print_alphabet_x10 - whatever
  */
 void print_alphabet_x10(void)
 {
-	for (int i = 0; i =< 9; i++)
+	int i;
+
+	for (i = 0; i < 10; i++)
 	{
 		print_alphabet(void);
 	}
