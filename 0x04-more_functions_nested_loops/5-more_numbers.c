41b7f60377e9c2c561016377b9607e4fe064cec4
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-17 16:25:16 -0700

    f

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - from 0 to 14 10 times
+ * Return:
+ */
+void more_numbers(void)
+{
+	int i;
+	int c;
+
+	while (i <= 10)
+	{
+		for (c = 48; c <= 63; c++)
+		{
+			_putchar(i);
+		}
+	i++;
+	}
+}
