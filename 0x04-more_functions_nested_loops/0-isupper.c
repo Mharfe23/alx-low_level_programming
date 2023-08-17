3a1fd2fbeabc023391017147c0529b04a66a7801
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-17 09:59:17 -0700

    f

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * _isupper - thats my fonction
+ *Return: 1 if c is uppercase or 0 if otherwise
+ *
+ */
+int _isupper(int c)
+{
+	if isupper(c)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
