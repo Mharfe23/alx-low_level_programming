704a55f07b4c53d93846dfea48cbeb67fd21c416
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-17 17:47:45 -0700

    f

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 != 0))
 			printf("Fizz");
-		if ((i % 3 != 0) && (i % 5 == 0))
+		else if ((i % 3 != 0) && (i % 5 == 0))
 			printf("Buzz");
 		else if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz");
