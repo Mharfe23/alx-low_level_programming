3ac05bfe62387db03e72175c396698649618b9a4
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-17 17:33:33 -0700

    f

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_square - square
+ * @size: integer
+ * Return:
+ */
+void print_square(int size)
+{
+	int i;
+	int c;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (c = 1; c <= size; c++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
