d3118c47671fb32135bc75f5126630396bc5a198
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-21 14:52:36 -0700

    ponters

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,13 +3,12 @@
  * swap_int - first task
  * @a:integer
  * @b:integer also
- * @m: temporary integer
  * Retrun:
  */
 void swap_int(int *a, int *b)
 {
 	int m;
-	
+
 	m = *a;
 	*a = *b;
 	*b = m;
