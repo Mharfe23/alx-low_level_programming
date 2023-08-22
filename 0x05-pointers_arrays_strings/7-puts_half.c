59d908594b8ee127cfdede120049e4a4663bffbc
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-22 15:32:47 -0700

    f

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - print the second half of a string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int lenght = 0;
+	int i;
+
+	while (str[lenght] != '\0')
+		lenght++;
+	lenght;
+
+	if (lenght % 2 == 0)
+	{
+		for (i = lenght / 2; i <= lenght; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = (lenght - 1) / 2; i <= lenght; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+}
