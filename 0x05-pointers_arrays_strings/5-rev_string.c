227a9b421c33950ddc9dec88dac7c7b347ab67b0
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-22 11:05:13 -0700

    f

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string
+ * @s:
+ */
+void rev_string(char *s)
+{
+	int l = 0;
+	char c;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	l--;
+	for (i = 0 ; i < l / 2; i++)
+	{
+		c = s[i];
+		s[i] = s[l - i - 1]
+		s[l - i - 1] = c
+	}
+}
+
+
