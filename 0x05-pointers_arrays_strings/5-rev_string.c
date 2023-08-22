5eaada0619d2f0f4f22866c76f29f77aad2adeab
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-22 11:37:51 -0700

    f

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,6 +7,7 @@ void rev_string(char *s)
 {
 	int l = 0;
 	char c;
+	char T[];
 	int i;
 
 	while (*s != '\0')
@@ -18,7 +19,9 @@ void rev_string(char *s)
 	for (i = 0 ; i < l / 2; i++)
 	{
 		c = s[i];
-		s[i] = s[l - i - 1];
-		s[l - i - 1] = c;
+		T[i] = s[l - i - 1];
+		T[l - i - 1] = c
 	}
+	for (i = 0; i < l; i++)
+		s[i] = T[i];
 }
