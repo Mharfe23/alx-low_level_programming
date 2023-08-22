d9b2dbaa3e9262ec91ffb29affb378c4abd2bb7f
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-22 16:25:55 -0700

    f

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - whaatever
+ * @dest: dest
+ * @src: src
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int j;
+	int lsrc = strlen(src)
+
+	for (i = 0; i <= lsrc; i++)
+		dest[i] = src[i];
+	return (dest);
+}
