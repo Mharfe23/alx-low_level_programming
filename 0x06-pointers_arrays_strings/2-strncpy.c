0e71e6166b464e34989adeaa78b40ed3b509c0e6
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-24 17:12:19 -0700

    f

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strncpy - ckkc
+ * Return: char
+ * @dest: f
+ * @src: m
+ * @n: integer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _strcmp - compare
+ * @s1: char
+ * @s2: char2
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	char *p = s1;
+	char *p1 = s2;
+
+	while (*p != '\0' && *p1 != '\0')
+	{
+		if (*p == *p1)
+			return (0);
+		else
+			break;
+	}
+		
+
+	else if (*s1 < *s2)
+		return (-15);
+	else if (*s1 > *s2)
+		return (15);
+	else
+	{
+		while (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+			if (*s1 < *s2)
+				return (-15);
+			else if (*s1 > *s2)
+				return (15);
