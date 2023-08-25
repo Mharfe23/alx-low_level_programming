937d1bc25a4d33e1c185644e9ce24686eecc43e5
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-25 10:40:38 -0700

    m

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - ll
+ * @a: pointer
+ * @n: integer
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp;
+	int count = 0;
+	int i = 0;
+
+	while (count < n)
+	{
+		tmp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i -1] = tmp;
+		i++;
+		count++;
+	}
+}
