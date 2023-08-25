4e4c33abe7468fc8dd1a67476ad591a7391fa35c
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-25 14:44:29 -0700

    f

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,10 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		if (str[i] == ' ' || str[i] == '\t' ||
+			       	str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' ||
+				str[i] == '?' || str[i] == '"' || str[i] == '(' ||
+			       	str[i] == ')' || str[i] == '{' || str[i] == '}')
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				str[i + 1] = str[i + 1] - 32;
