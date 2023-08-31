28005a4da0ecda012f15a2d0b156e050e33a42db
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-31 04:16:38 -0700

    g

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+/**
+ * devisor - number of divisor of a number
+ * @n: number
+ * @div: diviseur
+ * Return: number of divisor
+ */
+int devisor(int n, int div)
+{
+	if (div < n && n % div == 0)
+	{
+		return (devisor(n, div + 1) + 1)
+	}
+	else if (div == n)
+		return (1 + count);
+}
+/**
+ * is_prime_number - prime checker
+ * @n: int
+ * Return: 0 or 1
+ */
+int is_prime_number(int n)
+{
+	if (n == 1)
+		return (1);
+	else if (devisor(n, 1) > 2)
+		return (0);
+	else
+		return (1);
+}
