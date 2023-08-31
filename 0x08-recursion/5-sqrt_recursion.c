282ec24327c44d25e082fd0b34874c0b570299fe
Mharfe23 <fengiro.arafa@gmail.com>
2023-08-31 03:32:14 -0700

    g

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- * _sqrt_recursion - sqrt
- * @n: inte
- * secondary function
  * sqrtguess - guess all the numbers that maybe square roots
  * @guess: the variable that may be the natural square root of the number n
  * @n: integer
+ *
+ * _sqrt_recursion - sqrt
  * Return: inte
  */
 int sqrtguess(int n, int guess)
