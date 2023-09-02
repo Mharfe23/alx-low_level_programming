239500d166536aeac97dd24869720607b433600f
Mharfe23 <fengiro.arafa@gmail.com>
2023-09-02 16:24:49 -0700

    f

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+/**
+ * main - coin necessary
+ * @argc: argc
+ * @argv: argv
+ * Return: int
+ */
+int main (int argc, char *argv[])
+{
+	int cent;
+	int sum = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	cent = atoi(argv[1]);
+	while (cent <= 25)
+	{
+		sum++;
+		cent = cent / 25 + cent % 25;
+	}
+	while (cent <= 10)
+	{
+		sum++;
+		cent = cent / 10 + cent % 10;
+	}
+	while (cent <= 5)
+	{
+		sum++;
+		cent = cent / 5 + cent % 5;
+	}
+	while (cent <= 2)
+	{
+		sum++;
+		cent = cent / 2 + cent % 2;
+	}
+	if (cent == 1)
+		sum++;
+	printf("%d", sum);
+}
