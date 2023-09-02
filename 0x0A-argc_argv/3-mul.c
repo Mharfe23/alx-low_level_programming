2574e80dd03a8ab03123d8fc0efa1a1ba9941bc5
Mharfe23 <fengiro.arafa@gmail.com>
2023-09-02 15:06:43 -0700

    f

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - multiply two numbers
  * @argc: number of parametre
@@ -7,11 +8,12 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	int n1 = atoi(argv[1]);
+	int n2 = atoi(argv[2]);
 
 	if (argc == 3)
 	{
-		printf("%d\n", arg[1] * argv[2]);
+		printf("%d\n", n1 * n2);
 		return (0);
 	}
 	else
