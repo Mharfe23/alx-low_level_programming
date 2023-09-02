6116d5104709a6219153cb62890016c99e79d6a0
Mharfe23 <fengiro.arafa@gmail.com>
2023-09-02 14:37:23 -0700

    f

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * main - print programe name
  * @argc: argc
@@ -6,6 +7,7 @@
  */
 int main(int argc, char *argv[])
 {
+	(void) argc;
 	printf("%s", argv[0]);
 	return (0);
 }
